2073e301940ad98939ae90bdcda43fce2a8fb029
Umarfarook1912 <umarfarookj06@gmail.com>
2026-07-21 10:27:28 +0530

    currency bug fixed

--- src/app/(main)/ask-astrologer/checkout/page.tsx
+++ src/app/(main)/ask-astrologer/checkout/page.tsx
@@ -80,7 +80,9 @@ export default function AskAstrologerCheckoutPage() {
         ai_response: flow.ai_response,
         preferred_languages: flow.preferred_languages,
         currency,
-        ...(flow.muhurtha_result ? { muhurtha_result: flow.muhurtha_result } : {}),
+        ...(flow.muhurtha_result
+          ? { muhurtha_result: flow.muhurtha_result }
+          : {}),
       });
 
       await openRazorpayCheckout({
